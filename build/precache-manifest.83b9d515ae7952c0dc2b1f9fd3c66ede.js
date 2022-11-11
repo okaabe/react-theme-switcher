cd4a1ea0d72af3edf33eab965f3e54ca98d1c4af
tosuki <okaabe2006@gmail.com>
2022-11-11 12:00:55 -0300

    Added files

---
+++ build/precache-manifest.83b9d515ae7952c0dc2b1f9fd3c66ede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25387a913bb8259d45027ae6f19a511d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4529e49818e75189db09",
+    "url": "./static/js/2.9de51aa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9de51aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd1ff0df1a29f1f5ec4",
+    "url": "./static/js/main.507617e3.chunk.js"
+  },
+  {
+    "revision": "41f581d4464e91debe95",
+    "url": "./static/js/runtime-main.61f23d03.js"
+  }
+]);
